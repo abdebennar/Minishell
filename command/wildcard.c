457ed76b70ed1f321c40ca779b65595cc4067cd0
Badr Eddine El Oirghi <bel-oirg@e1r7p5.1337.ma>
2024-04-20 03:21:41 +0100

    anched dak chambrir ankhchih lik f l3ziza

--- command/wildcard.c
+++ command/wildcard.c
@@ -6,7 +6,7 @@
 /*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 01:44:16 by bel-oirg          #+#    #+#             */
-/*   Updated: 2024/04/20 01:32:55 by bel-oirg         ###   ########.fr       */
+/*   Updated: 2024/04/20 03:20:40 by bel-oirg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,37 +24,70 @@ bool check_pattern(char *str, char *pattern)
     return (false);
 }
 
-char	*_wildcard_(char *pattern)
+//checking the path .
+//check the quotes
+int	check_wildcard(char *str)
 {
-    struct dirent   *entry;
-    char            *extracted;
-    DIR             *dir;
+	int		inside_q;
+	char	*pattern;
+	char	*back_str;
+	int		index;
+	char	q;
 
-    dir = opendir(".");
-    if (!dir)
-    {
-        perror("opendir");
-        return (NULL);
-    }
-    extracted = NULL;
-    while (1)
-    {
-        entry = readdir(dir);
-        if (!entry)
-            break;
-        if (check_pattern(entry->d_name, pattern))
-        {
-            if (extracted)
-                extracted = ft_strjoin(extracted, "\t");
-            extracted = ft_strjoin(extracted, entry->d_name);
-        }
-    }
-	return (extracted);
+	inside_q = 0;
+	index = -1;
+	back_str = str;
+	pattern = my_malloc(ft_strlen(str), 1);
+	while (*str)
+	{
+		if (*str == '\'' && *(str + 1) == '\'')
+			str += 2;
+		else if (*str == '"' && *(str + 1) == '"')
+			str += 2;
+		else
+			pattern[++index] = *str++;
+	}
+	*str = 0;
+	str = back_str;
+	printf("this is str %s\n", pattern);
+	if (find_c(pattern, '"') || find_c(pattern, '\''))
+		return (0);
+	return (1);
 }
+// int main()
+// {
+//     // if (check_pattern("quoe", "*q*ye"))
+//     //     printf("YEpA\n");
+// 	char pattern[] = "*.c\"das\"";
+	
+//     printf("%d\n", check_wildcard(pattern));
+// }
 
-int main()
-{
-    // if (check_pattern("quoe", "*q*ye"))
-    //     printf("YEpA\n");
-    printf("%s\n", _wildcard_("*.c"));
-}
+// char	*_wildcard_(char *pattern)
+// {
+//     struct dirent   *entry;
+//     char            *extracted;
+//     DIR             *dir;
+
+//     dir = opendir(".");
+//     if (!dir)
+//     {
+//         perror("opendir");
+//         return (NULL);
+//     }
+//     extracted = NULL;
+//     while (1)
+//     {
+//         entry = readdir(dir);
+//         if (!entry)
+//             break;
+//         if (check_pattern(entry->d_name, pattern))
+//         {
+//             if (extracted)
+//                 extracted = ft_strjoin(extracted, "\t");
+//             extracted = ft_strjoin(extracted, entry->d_name);
+//         }
+//     }
+// 	return (extracted);
+// }
+
